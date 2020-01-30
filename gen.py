86cb0a7ed2aeb65bfcccabec067493ed3099bf0b
WangHaomao <haomao@DESKTOP-CNJD5KN.localdomain>
2020-01-31 07:59:51 +0800

    fix some bugs

--- gen.py
+++ gen.py
@@ -19,7 +19,7 @@ import os.path as osp
 from synthgen import *
 from common import *
 import wget, tarfile
-
+from common import img_debug
 ## Define some configuration variables:
 NUM_IMG = -1  # no. of images to use for generation (-1 to use all available):
 INSTANCE_PER_IMAGE = 1  # no. of times to use the same image
@@ -76,7 +76,7 @@ def add_res_to_db(imgname, res, db):
         db['data'][dname].attrs['txt'] = L
 
 
-def main(viz=False):
+def main(viz=True):
     # open databases:
     print(colorize(Color.BLUE, 'getting data..', bold=True))
     db = get_data()
@@ -98,39 +98,40 @@ def main(viz=False):
     RV3 = RendererV3(DATA_PATH, max_time=SECS_PER_IMG)
     for i in range(start_idx, end_idx):
         imname = imnames[i]
-        try:
-            # get the image:
-            img = Image.fromarray(db['image'][imname][:])
-            # get the pre-computed depth:
-            #  there are 2 estimates of depth (represented as 2 "channels")
-            #  here we are using the second one (in some cases it might be
-            #  useful to use the other one):
-            depth = db['depth'][imname][:].T
-            depth = depth[:, :, 1]
-            # get segmentation:
-            seg = db['seg'][imname][:].astype('float32')
-            area = db['seg'][imname].attrs['area']
-            label = db['seg'][imname].attrs['label']
+        # try:
+        # get the image:
+        img = Image.fromarray(db['image'][imname][:])
+        # get the pre-computed depth:
+        #  there are 2 estimates of depth (represented as 2 "channels")
+        #  here we are using the second one (in some cases it might be
+        #  useful to use the other one):
+        depth = db['depth'][imname][:].T
+        depth = depth[:, :, 1]
+        # get segmentation:
+        seg = db['seg'][imname][:].astype('float32')
+        area = db['seg'][imname].attrs['area']
+        label = db['seg'][imname].attrs['label']
 
-            # re-size uniformly:
-            sz = depth.shape[:2][::-1]
-            img = np.array(img.resize(sz, Image.ANTIALIAS))
-            seg = np.array(Image.fromarray(seg).resize(sz, Image.NEAREST))
+        # re-size uniformly:
+        sz = depth.shape[:2][::-1]
+        img = np.array(img.resize(sz, Image.ANTIALIAS))
+        seg = np.array(Image.fromarray(seg).resize(sz, Image.NEAREST))
 
-            print(colorize(Color.RED, '%d of %d' % (i, end_idx - 1), bold=True))
-            res = RV3.render_text(img, depth, seg, area, label,
-                                  ninstance=INSTANCE_PER_IMAGE, viz=viz)
-            if len(res) > 0:
-                # non-empty : successful in placing text:
-                add_res_to_db(imname, res, out_db)
-            # visualize the output:
-            if viz:
-                if 'q' in input(colorize(Color.RED, 'continue? (enter to continue, q to exit): ', True)):
-                    break
-        except:
-            traceback.print_exc()
-            print(colorize(Color.GREEN, '>>>> CONTINUING....', bold=True))
-            continue
+        print(colorize(Color.RED, '%d of %d' % (i, end_idx - 1), bold=True))
+        res = RV3.render_text(img, depth, seg, area, label,
+                              ninstance=INSTANCE_PER_IMAGE, viz=viz)
+        print(res)
+        if len(res) > 0:
+            # non-empty : successful in placing text:
+            add_res_to_db(imname, res, out_db)
+        # visualize the output:
+        if viz:
+            if 'q' in input(colorize(Color.RED, 'continue? (enter to continue, q to exit): ', True)):
+                break
+        # except:
+        #     traceback.print_exc()
+        #     print(colorize(Color.GREEN, '>>>> CONTINUING....', bold=True))
+        #     continue
     db.close()
     out_db.close()
 
